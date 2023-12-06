756fc0f4458300d0e83ef0e2a50a8aca13426207
SpaceXM <altairaerospace@gmail.com>
2023-12-06 18:32:44 +0100

    3.1.13.1

--- lib/src/ui/intro_button.dart
+++ lib/src/ui/intro_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class IntroButton extends StatelessWidget {
+class IntroButton extends StatefulWidget {
   final Widget child;
   final VoidCallback? onPressed;
   final ButtonStyle? style;
@@ -16,34 +16,44 @@ class IntroButton extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<IntroButton> createState() => _IntroButtonState();
+}
+
+class _IntroButtonState extends State<IntroButton> {
+  bool is_on_focus = false;
+  @override
   Widget build(BuildContext context) {
     return MergeSemantics(
       child: Semantics(
-        label: semanticLabel,
-        button: true,
-        child: 
-        Focus(
-          onKey: (node, event) {
-          if (event is RawKeyDownEvent) {
-            if (event.logicalKey.keyLabel == 'Select' || event.logicalKey.keyLabel == 'Game Button A' || event.logicalKey == LogicalKeyboardKey.select) {
-              onPressed!();
-              return KeyEventResult.handled;
-            }
-            return KeyEventResult.ignored;
-          }
-          return KeyEventResult.ignored;
-        },
-          child: 
-        TextButton(
-          onPressed: onPressed,
-          child: child,
-          style: TextButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
+          label: widget.semanticLabel,
+          button: true,
+          child: Focus(
+            onFocusChange: (value) => {
+              setState(() {
+                is_on_focus = value;
+              })
+            },
+            onKey: (node, event) {
+              if (event is RawKeyDownEvent) {
+                if (event.logicalKey.keyLabel == 'Select' || event.logicalKey.keyLabel == 'Game Button A' || event.logicalKey == LogicalKeyboardKey.select) {
+                  widget.onPressed!();
+                  return KeyEventResult.handled;
+                }
+                return KeyEventResult.ignored;
+              }
+              return KeyEventResult.ignored;
+            },
+            child: TextButton(
+              onPressed: widget.onPressed,
+              child: widget.child,
+              style: TextButton.styleFrom(
+                backgroundColor: is_on_focus ? Colors.red : Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ).merge(widget.style),
             ),
-          ).merge(style),
-        ),
-      )),
+          )),
     );
   }
 }
